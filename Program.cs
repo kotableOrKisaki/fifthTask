ef7cea64cfe0a7f83050b4af8955cc73f3dece27
kotableOrKisaki <super_misha05@mail.ru>
2022-08-24 20:17:06 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace fifthTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Kisaki";
+            string lastName = "Суня";
+            Console.WriteLine(firstName + lastName);
+
+            (firstName, lastName) = (lastName, firstName);
+            Console.WriteLine(firstName + lastName);
+        }
+    }
+}
